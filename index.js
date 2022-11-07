f0017e969a2607838d4b1b738420bac745056280
Rifat7432 <mdr341762@gmail.com>
2022-11-08 02:24:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middle wears
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("backend is working");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rdtrwss.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    const servicesCollection = client.db("Assignment-11").collection("services");
+    app.get("/services/3", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+};
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("server is running on port : ", port);
+});
